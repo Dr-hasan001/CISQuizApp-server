83aafa6ab841f1b2542e608ea4de8f3b6bbfbf44
Dr-hasan001 <drha2757@gmail.com>
2024-07-11 10:59:01 +0300

    First server Upload

---
+++ database/data.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    question: "1.These are not my students. Those are ....",
+    options: ["Me", "My", "Mine"],
+  },
+
+  {
+    id: 2,
+    question: "2.... Name is Ali",
+    options: ["My", "Me", "I"],
+  },
+
+  {
+    id: 3,
+    question: "3.Your..... is very good",
+    options: ["grade", "cold", "swim"],
+  },
+
+  {
+    id: 4,
+    question: "4.I know Asmaa. She is an old ......",
+    options: ["friend of mine", "me friend", "my friend"],
+  },
+
+  {
+    id: 5,
+    question: "5.The... grades are very good  ",
+    options: ["Student's", "Students'", "student is"],
+  },
+];
+
+export const answers = [2, 0, 0, 0, 1];
+
+/**IMPORTANT NOTE/!!! Each time you add a new question you must put the index
+  of the answer of your question in the answers arrray !!!!!!!!!!!!!*/
